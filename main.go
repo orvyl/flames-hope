0e65daac10895eeb5bd8e79db764c206011e0367
orvyl <OFTumaneng@smartemoneyinc.com>
2016-01-06 12:11:13 +0800

    get random results

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+)
+
+const flamesHopeStr = "flameshope"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("*** Welcome to Flames Hope! ***")
+
+	fmt.Print("Please enter your name: ")
+	var name string
+	fmt.Scanf("%s", &name)
+
+	fmt.Print("Please enter your crush's name: ")
+	var crushName string
+	fmt.Scanf("%s", &crushName)
+
+	result := "Walang forever sa inyo :("
+
+	if rand.Int31() % 2 == 0 {
+		result = strconv.Itoa(rand.Intn(100)) + "%"
+	}
+
+	fmt.Println("RESULT: " + result)
 }
